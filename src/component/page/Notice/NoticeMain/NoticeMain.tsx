9ec25cc473669625e68311a9d411e02ed26089e9
worney87 <worney87@gmail.com>
2024-11-13 15:02:19 +0900

    getNoticeList

--- src/component/page/Notice/NoticeMain/NoticeMain.tsx
+++ src/component/page/Notice/NoticeMain/NoticeMain.tsx
@@ -1,44 +1,82 @@
-import { useLocation } from 'react-router-dom';
-import { StyledTable, StyledTd, StyledTh } from '../../../common/styled/StyledTable';
-import { useEffect, useState } from 'react';
-import axios, { AxiosResponse } from 'axios';
-import { useRecoilState } from 'recoil';
-import { Protal } from '../../../common/potal/Portal';
-import { NoticeModal } from '../NoticeModal/NoticeModal';
-import { Button } from 'react-bootstrap';
+import { Link, useLocation } from "react-router-dom";
+import {
+  StyledTable,
+  StyledTd,
+  StyledTh,
+} from "../../../common/styled/StyledTable";
+import { useEffect, useState } from "react";
+import axios, { AxiosResponse } from "axios";
+import { useRecoilState } from "recoil";
+import { Protal } from "../../../common/potal/Portal";
+import { NoticeModal } from "../NoticeModal/NoticeModal";
+import { Button } from "react-bootstrap";
 
 interface INoitce {
-    author: string;
-    content: string;
-    createdDate: string;
-    fileExt: string | null;
-    fileName: string | null;
-    fileSize: number;
-    logicalPath: string | null;
-    noticeIdx: number;
-    phsycalPath: string | null;
-    title: string;
-    updatedDate: string | null;
+  author: string;
+  content: string;
+  createdDate: string;
+  fileExt: string | null;
+  fileName: string | null;
+  fileSize: number | null;
+  logicalPath: string | null;
+  noticeIdx: number;
+  phsycalPath: string | null;
+  title: string;
+  updatedDate: string | null;
 }
 
 export const NoticeMain = () => {
-    return (
-        <>
-            총 갯수 : 0 현재 페이지 : 0
-            <StyledTable>
-                <thead>
-                    <tr>
-                        <StyledTh size={5}>번호</StyledTh>
-                        <StyledTh size={50}>제목</StyledTh>
-                        <StyledTh size={20}>등록일</StyledTh>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <StyledTd colSpan={3}>데이터가 없습니다.</StyledTd>
-                    </tr>
-                </tbody>
-            </StyledTable>
-        </>
-    );
+  const { search } = useLocation();
+  const [noticeList, setNoticeList] = useState<INoitce[]>();
+  const [listCount, setListCount] = useState<number>(0);
+
+  useEffect(() => {
+    searchNoticeList();
+  }, [search]);
+
+  const searchNoticeList = (currentPage?: number) => {
+    currentPage = currentPage || 1;
+    const searchParam = new URLSearchParams(search);
+    searchParam.append("currentPage", currentPage.toString());
+    searchParam.append("pageSize", "5");
+
+    axios.post("/board/noticeListJson.do", searchParam).then((res) => {
+      setNoticeList(res.data.notice);
+      setListCount(res.data.noticeCnt);
+    });
+  };
+
+  return (
+    <>
+      총 갯수 : {listCount} 현재 페이지 : 0
+      <StyledTable>
+        <thead>
+          <tr>
+            <StyledTh size={5}>번호</StyledTh>
+            <StyledTh size={50}>제목</StyledTh>
+            <StyledTh size={10}>작성자</StyledTh>
+            <StyledTh size={20}>등록일</StyledTh>
+          </tr>
+        </thead>
+        <tbody>
+          {noticeList && noticeList.length > 0 ? (
+            noticeList?.map((notice, index) => {
+              return (
+                <tr key={index}>
+                  <StyledTd>{notice.noticeIdx}</StyledTd>
+                  <StyledTd><Link to="">{notice.title}</Link></StyledTd>
+                  <StyledTd>{notice.author}</StyledTd>
+                  <StyledTd>{notice.createdDate}</StyledTd>
+                </tr>
+              );
+            })
+          ) : (
+            <tr>
+              <StyledTd colSpan={3}>데이터가 없습니다.</StyledTd>
+            </tr>
+          )}
+        </tbody>
+      </StyledTable>
+    </>
+  );
 };
--- src/component/page/Notice/NoticeSearch/NoticeSearch.tsx
+++ src/component/page/Notice/NoticeSearch/NoticeSearch.tsx
@@ -1,39 +1,40 @@
-import { useEffect, useRef, useState } from 'react';
-import { NoticeSearchStyled } from './styled';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { Button } from '../../../common/Button/Button';
+import { NoticeSearchStyled } from "./styled";
+import { Button } from "../../../common/Button/Button";
+import { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const NoticeSearch = () => {
-    const [startDate, setStartDate] = useState<string>();
-    const [endDate, setEndDate] = useState<string>();
-    const title = useRef<HTMLInputElement>(null);
-    const navigate = useNavigate();
-    const location = useLocation();
+  const title = useRef<HTMLInputElement>();
+  const [startDate, setStartDate] = useState<string>();
+  const [endDate, setEndDate] = useState<string>();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        location.search && navigate(location.pathname, { replace: true });
-    }, [navigate]);
+  useEffect(() => {
+    window.location.search && navigate(window.location.pathname, { replace: true });
+  }, [navigate]);
 
-    const handlerSearch = () => {
-        // 검색 버튼을 누르면, 조회가 된다.
-        const query: string[] = [];
-        !title.current?.value || query.push(`searchTitle=${title.current?.value}`);
-        !startDate || query.push(`startDate=${startDate}`);
-        !endDate || query.push(`endDate=${endDate}`);
+  const handlerSearch = () => {
+    const query: string[] = [];
+    !title.current.value || query.push(`searchTitle=${title.current.value}`);
+    !startDate || query.push(`searchStDate=${startDate}`);
+    !endDate || query.push(`searchEdDate=${endDate}`);
 
-        const queryString = query.length > 0 ? `?${query.join('&')}` : '';
-        navigate(`/react/board/notice.do${queryString}`);
-    };
+    const queryString = query.length > 0 ? `?${query.join("&")}` : "";
+    //navigate(`/react/board/notice.do${queryString}`); 절대경로
+    navigate(queryString); // 상대경로
+  };
 
-    return (
-        <NoticeSearchStyled>
-            <div className="input-box">
-                <input ref={title}></input>
-                <input type="date" onChange={(e) => setStartDate(e.target.value)}></input>
-                <input type="date" onChange={(e) => setEndDate(e.target.value)}></input>
-                <Button onClick={handlerSearch}>검색</Button>
-                {/* <Button onClick={handlerModal}>등록</Button> */}
-            </div>
-        </NoticeSearchStyled>
-    );
+  return (
+    <NoticeSearchStyled>
+      <div className="input-box">
+        <input ref={title}></input>
+        <input
+          type="date"
+          onChange={(e) => setStartDate(e.target.value)}
+        ></input>
+        <input type="date" onChange={(e) => setEndDate(e.target.value)}></input>
+        <Button onClick={handlerSearch}>검색</Button>
+      </div>
+    </NoticeSearchStyled>
+  );
 };
